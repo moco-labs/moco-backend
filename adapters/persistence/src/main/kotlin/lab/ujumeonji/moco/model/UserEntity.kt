7d2f9c8dde9eb19d3aa8af5286ce43c2c10f1893
otter.moon <404err@naver.com>
2025-06-08 02:10:04 +0900

    fix: User entity 식별자 기본값 처리

--- adapters/persistence/src/main/kotlin/lab/ujumeonji/moco/model/UserEntity.kt
+++ adapters/persistence/src/main/kotlin/lab/ujumeonji/moco/model/UserEntity.kt
@@ -5,12 +5,13 @@ import org.springframework.data.annotation.TypeAlias
 import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
+import java.util.UUID
 
 @Document(collection = "users")
 @TypeAlias("User")
 data class UserEntity(
     @Id
-    val id: String? = null,
+    val id: String = UUID.randomUUID().toString(),
     val name: String,
     @Indexed(unique = true)
     val email: String,
