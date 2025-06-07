ff04401813a988ea54836be95aa1688cf9b950a2
otter.moon <404err@naver.com>
2025-06-07 17:54:48 +0900

    chore: Dockerizing 설정 구성

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    id("com.google.cloud.tools.jib") version "3.4.5"
     id("org.jetbrains.kotlin.jvm")
     id("org.jetbrains.kotlin.plugin.spring")
     id("org.springframework.boot")
@@ -25,4 +26,52 @@ dependencies {
 
 tasks.getByName("bootJar") {
     enabled = true
+}
+
+jib {
+    from {
+        image = "eclipse-temurin:21-jre-alpine"
+        platforms {
+            platform {
+                architecture = "amd64"
+                os = "linux"
+            }
+            platform {
+                architecture = "arm64"
+                os = "linux"
+            }
+        }
+    }
+    to {
+        image = System.getenv("DOCKER_HUB_IMAGE")
+        tags = setOf("latest", version.toString())
+        auth {
+            username = System.getenv("DOCKER_HUB_USERNAME")
+            password = System.getenv("DOCKER_HUB_PASSWORD")
+        }
+    }
+    container {
+        mainClass = "lab.ujumeonji.moco.MocoApiApplication"
+        jvmFlags = listOf(
+            "-Xms1024m",
+            "-Xmx1024m",
+            "-XX:+UseContainerSupport",
+            "-XX:MaxRAMPercentage=75.0",
+            "-Djava.security.egd=file:/dev/./urandom"
+        )
+        ports = listOf(System.getenv("PORT") ?: "8080")
+        environment = mapOf(
+            "SPRING_PROFILES_ACTIVE" to System.getenv("APP_PROFILE"),
+            "LANG" to "en_US.UTF-8"
+        )
+        creationTime.set("USE_CURRENT_TIMESTAMP")
+        workingDirectory = "/app"
+        user = "1000:1000"
+        volumes = listOf("/app/logs")
+        labels = mapOf(
+            "maintainer" to "webdev0594",
+            "version" to version.toString(),
+            "description" to "Moco Application"
+        )
+    }
 }
